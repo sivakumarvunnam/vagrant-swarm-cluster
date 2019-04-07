10747b5d593a10bff54fb33fcc199ad949c35acc
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2019-04-07 20:21:43 +0000

    script to create swarm cluster

---
+++ start_cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Starting manager..."
+docker -H tcp://10.0.1.10:2375 swarm init --advertise-addr 10.0.1.10
+
+echo "Retrieve manager token..."
+TOKEN=$(docker -H tcp://10.0.1.10:2375 swarm join-token -q worker)
+
+echo "Starting node..."
+docker -H tcp://10.0.1.11:2375 swarm join --token "${TOKEN}" 10.0.1.10:2377
+
+docker -H tcp://10.0.1.10:2375 info
